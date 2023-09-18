d28b8191bc030c323b86204c28fa39b49ecacf91
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2023-09-19 01:45:02 +0700

    comment doc riverpod V2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-// import 'package:demo_ft_state_management/demo_GetX_viblo/demo_GetX.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:demo_ft_state_management/demo_Riverpod/index.dart';
+
+// import 'package:demo_ft_state_management/demo_GetX_viblo/demo_GetX.dart';
+// import 'package:demo_ft_state_management/demo_Riverpod/index.dart';
 
 // import 'package:demo_ft_state_management/demo_variable_global/main.dart';
+
+import 'package:demo_ft_state_management/doc_riverpod2_0/main.dart';
 
 void main() {
   // runApp(MyApp());
