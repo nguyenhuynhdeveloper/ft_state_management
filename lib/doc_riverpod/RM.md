270f35fdb23e2cd63d9592b0e0572bdaffde5e35
nguyenhuynhdeveloper <nguyenhuynhdeveloper>
2023-09-19 15:14:18 +0700

    change reiverpod

---
+++ lib/doc_riverpod/RM.md
@@ -0,0 +1,3 @@
+## https://codewithandrea.com/articles/flutter-riverpod-async-notifier/
+
+Sử dụng cú pháp Riverpod mới, chúng tôi sử dụng chú thích @riverpod và cho phép build_runner tạo tất cả các nhà cung cấp một cách nhanh chóng.
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:demo_ft_state_management/demo_GetX_viblo/demo_GetX.dart';
-import 'package:demo_ft_state_management/demo_Riverpod/index.dart';
+// import 'package:demo_ft_state_management/demo_Riverpod/index.dart';
 
-import 'package:demo_ft_state_management/doc_riverpod2_0/main.dart';
+import 'package:demo_ft_state_management/doc_riverpod/main.dart';
 
 void main() {
   // runApp(MyApp());
